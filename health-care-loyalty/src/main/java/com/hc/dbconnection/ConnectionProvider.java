28cf26b604df53e3549b2d69f3f5975ba8505981
hiddenleaftech <79269964+hiddenleaftech@users.noreply.github.com>
2021-02-24 13:05:45 +0530

    Delete health-care-loyalty directory

--- health-care-loyalty/src/main/java/com/hc/dbconnection/ConnectionProvider.java
+++
@@ -1,8 +0,0 @@
-package com.hc.dbconnection;
-
-public interface ConnectionProvider {
-	
-	String username="postgres";
-	String password="password";
-	String conURL="jdbc:postgresql://localhost:5432/loyalty"; 
-}
